2f0f0d3b3c0ce0ecba15115412f081b1c08aed6c
Schtitle <Steidley.winston@gmail.com>
2024-03-08 13:51:16 -0600

    syncing files to main to pull, including schema folder with resolvers, typedef, and index. as well as updating model and seed to include questDescription

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -2,17 +2,15 @@ const { faker } = require('@faker-js/faker');
 const { User, QuestLog, Quest, QuestLocation, QuestItems } = require('../models');
 const db = require('../models');
 
-
 let userIdCounter = 1;
 let questlogIdCounter = 1;
 let questIdCounter = 1;
 let questLocationIdCounter = 1;
 let questItemIdCounter = 1;
 
-
 const seedDatabase = async () => {
-
   await db.sequelize.sync({ force: true });
+
   // Create a user
   const user = await User.create({
     user_id: userIdCounter++,
@@ -25,13 +23,13 @@ const seedDatabase = async () => {
   const questLog = await QuestLog.create({
     quest_log_id: questlogIdCounter++,
     user_id: user.user_id,
-    
   });
 
   // Create a quest for the quest log
   const quest = await Quest.create({
     quest_id: questIdCounter++,
     questName: faker.lorem.words(3),
+    questDescription: faker.lorem.paragraph(), // Adding description
     quest_log_id: questLog.quest_log_id,
   });
 
