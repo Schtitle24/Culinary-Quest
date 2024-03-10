ca723392b967050c131a73406090c78469779867
John D <johnd@Johns-MBP.attlocal.net>
2024-03-09 18:45:11 -0600

    login form sign up form auth page and APP jsx

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,27 +1,71 @@
-// import React from 'react';
-
-// function LoginForm() {
-//     return (
-//         <div className="container">
-//             <h2>Login Form</h2>
-//             <form>
-//                 <div className="form-group">
-//                     <label htmlFor="exampleInputEmail2">Email address</label>
-//                     <input type="email" className="form-control" id="exampleInputEmail2" aria-describedby="emailHelp" placeholder="Enter email" />
-//                     <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-//                 </div>
-//                 <div className="form-group">
-//                     <label htmlFor="exampleInputPassword2">Password</label>
-//                     <input type="password" className="form-control" id="exampleInputPassword2" placeholder="Password" />
-//                 </div>
-//                 <div className="form-group form-check">
-//                     <input type="checkbox" className="form-check-input" id="exampleCheck2" />
-//                     <label className="form-check-label" htmlFor="exampleCheck2">Check me out</label>
-//                 </div>
-//                 <button type="submit" className="btn btn-primary">Submit</button>
-//             </form>
-//         </div>
-//     );
-// }
-
-// export default LoginForm;
+import React, { useState } from 'react';
+import { Form, Button, Alert } from 'react-bootstrap';
+//import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
+const LoginForm = () => {
+  const [userFormData, setUserFormData] = useState({ email: '', password: '' });
+  const [validated, setValidated] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+  //const [loginUser] = useMutation(LOGIN_USER);
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUserFormData({ ...userFormData, [name]: value });
+  };
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+      setValidated(true);
+      return;
+    }
+     try {
+    //   const { data } = await loginUser({
+    //     variables: { ...userFormData }
+    //   });
+      const { token } = data.login;
+      Auth.login(token);
+    } catch (err) {
+      console.error(err);
+      setShowAlert(true);
+    }
+  };
+  return (
+    <>
+      <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+        <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
+          Invalid email or password. Please try again.
+        </Alert>
+        <Form.Group controlId='formBasicEmail' className='mb-3'>
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type='email'
+            placeholder='Enter email'
+            name='email'
+            value={userFormData.email}
+            onChange={handleInputChange}
+            required
+          />
+          <Form.Control.Feedback type='invalid'>Please enter a valid email.</Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group controlId='formBasicPassword' className='mb-3'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type='password'
+            placeholder='Password'
+            name='password'
+            value={userFormData.password}
+            onChange={handleInputChange}
+            required
+          />
+          <Form.Control.Feedback type='invalid'>Please enter your password.</Form.Control.Feedback>
+        </Form.Group>
+        <Button variant='primary' type='submit'>
+          Log In
+        </Button>
+      </Form>
+    </>
+  );
+};
+export default LoginForm;
