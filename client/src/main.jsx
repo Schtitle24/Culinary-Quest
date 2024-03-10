629da93a1e644a32b0ca78883957869008400878
John D <johnd@Johns-MBP.attlocal.net>
2024-03-09 23:05:43 -0600

    start quest page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,32 @@
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App.jsx'
-import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css'; 
-// import 'bootstrap/dist/css/react-bootstrap'; 
-
+import AuthPage from "./pages/AuthPage.jsx"
+import Home from "./pages/Home.jsx"
+const newRoute = createBrowserRouter([
+  {
+    path: '/', //sets landing page and base url
+    element: <App />,
+    errorElement: <h1 className='display-2'>Wrong page!</h1>,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: '/AuthPage',
+        element: <AuthPage />
+      },
+    //   {
+    //     path: '/SingleQuest',
+    //     element: <SingleQuest />
+    //   },
+    //   {
+    //     path: '/StartQuest',
+    //     element: <StartQuest />
+    //   },
+    ]
+  }
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
-
-        <App />
-);
--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -1,48 +1,48 @@
 const { User, QuestLog, Quest, QuestLocation, QuestItems } = require('../models');
 
-// query our quest object a Quest
-// query out quest log which will identical to the 21-20 example
-//query for card with qustlocation, username, and description
+// // query our quest object a Quest
+// // query out quest log which will identical to the 21-20 example
+// //query for card with qustlocation, username, and description
 
-//  upodate mutation will be for checckboxes.  
-// allow users to delete entire quests. 
-const resolvers = {
-  Query: {
-    // Query to get all quests associated with a user_id
-    QuestLog: async (parent, { user_id }) => {
-      return QuestLog.findOne({
-        where: { user_id },
-        include: [{ model: Quest, include: QuestLocation }],
-      });
-    },
+// //  upodate mutation will be for checckboxes.  
+// // allow users to delete entire quests. 
+// const resolvers = {
+//   Query: {
+//     // Query to get all quests associated with a user_id
+//     QuestLog: async (parent, { user_id }) => {
+//       return QuestLog.findOne({
+//         where: { user_id },
+//         include: [{ model: Quest, include: QuestLocation }],
+//       });
+//     },
 
-    // Query to get a single quest by quest_id
-    Quest: async (parent, { quest_id }) => {
-      return Quest.findOne({user_id: quest_id });
-    },
+//     // Query to get a single quest by quest_id
+//     Quest: async (parent, { quest_id }) => {
+//       return Quest.findOne({user_id: quest_id });
+//     },
 
-    // Query to get quest card details
-    QuestCard: async (parent, { quest_id }) => {
-      const quest = await Quest.findByPk(quest_id, {
-        include: QuestLocation,
-      });
-      if (!quest) {
-        throw new Error('Quest not found');
-      }
-      return {
-        questName: quest.questName,
-        questLocation: quest.QuestLocation.questLocation,
-        description: quest.description, // Assuming description is a field in Quest model
-      };
-    },
-  },
+//     // Query to get quest card details
+//     QuestCard: async (parent, { quest_id }) => {
+//       const quest = await Quest.findByPk(quest_id, {
+//         include: QuestLocation,
+//       });
+//       if (!quest) {
+//         throw new Error('Quest not found');
+//       }
+//       return {
+//         questName: quest.questName,
+//         questLocation: quest.QuestLocation.questLocation,
+//         description: quest.description, // Assuming description is a field in Quest model
+//       };
+//     },
+//   },
 
-  Mutation: {
-    // Mutation to add a new quest
-    addQuest: async (_, { questName, quest_log_id }) => {
-      return Quest.create({ questName, quest_log_id });
-    },
-  },
-};
+//   Mutation: {
+//     // Mutation to add a new quest
+//     addQuest: async (_, { questName, quest_log_id }) => {
+//       return Quest.create({ questName, quest_log_id });
+//     },
+//   },
+// };
 
-module.exports = resolvers;
+// module.exports = resolvers;
