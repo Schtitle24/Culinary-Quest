59abd5200a523515a363b9d50a3f6fc8733f561a
John D <johnd@Johns-MBP.attlocal.net>
2024-03-09 22:20:29 -0600

    updates

--- client/src/pages/AuthPage.jsx
+++ client/src/pages/AuthPage.jsx
@@ -17,8 +17,8 @@ import LoginForm from '../components/LoginForm';
         );
       };
 export default AuthPage;
-6:33
-APP JSX Code . import './App.css';
+
+import './App.css';
 import { Outlet } from 'react-router-dom';
 // Important for API Consumption: To enable interaction with our GraphQL API on the front end, we utilize these tools to develop the client-side behavior
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
