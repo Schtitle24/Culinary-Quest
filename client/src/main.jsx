d7c729b73799939d1ff7722a84cec865b34428f3
Schtitle24 <153530625+Schtitle24@users.noreply.github.com>
2024-03-09 18:51:01 -0600

    transfering code form auth to main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,21 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css'; 
-// import 'bootstrap/dist/css/react-bootstrap'; 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-
-        <App />
-);
+import { Outlet } from 'react-router-dom';
+// Important for API Consumption: To enable interaction with our GraphQL API on the front end, we utilize these tools to develop the client-side behavior
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import Header from './pages/Header';
+import Footer from './pages/Footer';
+const client = new ApolloClient({
+    uri: '/graphql',
+    cache: new InMemoryCache(),
+  });
+function App() {
+  return (
+    <ApolloProvider client={client}>
+    <div className="container" >
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+    </ApolloProvider>
+  );
+}
+export default App;
--- client/src/pages/AuthPage.jsx
+++ client/src/pages/AuthPage.jsx
@@ -17,26 +17,3 @@ import LoginForm from '../components/LoginForm';
         );
       };
 export default AuthPage;
-6:33
-APP JSX Code . import './App.css';
-import { Outlet } from 'react-router-dom';
-// Important for API Consumption: To enable interaction with our GraphQL API on the front end, we utilize these tools to develop the client-side behavior
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-import Header from './pages/Header';
-import Footer from './pages/Footer';
-const client = new ApolloClient({
-    uri: '/graphql',
-    cache: new InMemoryCache(),
-  });
-function App() {
-  return (
-    <ApolloProvider client={client}>
-    <div className="container" >
-      <Header />
-      <Outlet />
-      <Footer />
-    </div>
-    </ApolloProvider>
-  );
-}
-export default App;
