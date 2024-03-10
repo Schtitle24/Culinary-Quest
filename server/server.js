1ddd24443e39bfe3af167633b6f9155ebbac55a0
diamond clements <diamondclements32@gmail.com>
2024-03-09 19:29:37 -0500

    changed name to schema in server.js file

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const { authMiddleware } = require('./utils/auth');
 
 
-const { typeDefs, resolvers } = require('./schemas');
+const { typeDefs, resolvers } = require('./schema');
 const sequelize = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
