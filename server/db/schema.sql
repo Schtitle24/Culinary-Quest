87cad8287c0d4ec818f016c37b38909a547e5e00
Schtitle24 <153530625+Schtitle24@users.noreply.github.com>
2024-03-11 13:29:31 -0500

    debuggin all models that were commented out.

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,2 +1,2 @@
--- DROP DATABASE IF EXISTS quest_db;
--- CREATE DATABASE quest_db;
+DROP DATABASE IF EXISTS quest_db;
+CREATE DATABASE quest_db;
--- server/models/Quest.js
+++ server/models/Quest.js
@@ -1,41 +1,40 @@
-// models/Quest.js
-// const { Model, DataTypes } = require('sequelize');
-// const sequelize = require('../config/connection');
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
 
-// class Quest extends Model {}
+class Quest extends Model {}
 
-// Quest.init(
-//   {
-//     quest_id: {
-//       type: DataTypes.INTEGER,
-//       allowNull: false,
-//       primaryKey: true,
-//       autoIncrement: true,
-//     },
-//     questName: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//     questDescription: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//     quest_log_id: {
-//       type: DataTypes.INTEGER,
-//       allowNull: false,
-//       references: {
-//         model: 'QuestLog', 
-//         key: 'quest_log_id', 
-//       },
-//     },
-//   },
-//   {
-//     sequelize,
-//     timestamps: false,
-//     freezeTableName: true,
-//     underscored: true,
-//     modelName: 'Quest',
-//   }
-// );
+Quest.init(
+  {
+    quest_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    questName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    questDescription: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    quest_log_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'QuestLog', 
+        key: 'quest_log_id', 
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'Quest',
+  }
+);
 
-// module.exports = Quest;
+module.exports = Quest;
