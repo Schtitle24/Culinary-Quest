64c8f14767eda33035a497e911aadb353a4a6dae
diamond clements <diamondclements32@gmail.com>
2024-03-11 14:33:34 -0400

    tried debugging

--- server/models/QuestLog.js
+++ server/models/QuestLog.js
@@ -1,37 +1,37 @@
 // models/QuestLog.js
-// const { Model, DataTypes } = require('sequelize');
-// const sequelize = require('../config/connection');
+// // const { Model, DataTypes } = require('sequelize');
+// // const sequelize = require('../config/connection');
 
-// class QuestLog extends Model {}
+// // class QuestLog extends Model {}
 
-// QuestLog.init(
-//   {
-//     quest_log_id: {
-//       type: DataTypes.INTEGER,
-//       allowNull: false,
-//       primaryKey: true,
-//       autoIncrement: true,
-//     },
-//     user_id: { 
-//       type: DataTypes.INTEGER,
-//       allowNull: false,
-//       references: {
-//         model: 'User',
-//         key: 'user_id', 
-//       },
-//     },
-//     completionDate: {
-//       type: DataTypes.DATE,
-//       allowNull: true,
-//     },
-//   },
-//   {
-//     sequelize,
-//     timestamps: false,
-//     freezeTableName: true,
-//     underscored: true,
-//     modelName: 'QuestLog',
-//   }
-// );
+// // QuestLog.init(
+// //   {
+// //     quest_log_id: {
+// //       type: DataTypes.INTEGER,
+// //       allowNull: false,
+// //       primaryKey: true,
+// //       autoIncrement: true,
+// //     },
+// //     user_id: { 
+// //       type: DataTypes.INTEGER,
+// //       allowNull: false,
+// //       references: {
+// //         model: 'User',
+// //         key: 'user_id', 
+// //       },
+// //     },
+// //     completionDate: {
+// //       type: DataTypes.DATE,
+// //       allowNull: true,
+// //     },
+// //   },
+// //   {
+// //     sequelize,
+// //     timestamps: false,
+// //     freezeTableName: true,
+// //     underscored: true,
+// //     modelName: 'QuestLog',
+// //   }
+// // );
 
-// module.exports = QuestLog;
+// // module.exports = QuestLog;
