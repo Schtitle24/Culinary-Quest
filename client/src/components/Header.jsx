350d33c90b1ddc8d5508c6d14ee2133aca102138
Bdoherty1 <bdoherty988@gmail.com>
2024-03-11 10:46:40 -0500

    new package.json and homepage updated

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-
 const ButtonSection = styled.div`
   display: flex;
   justify-content: flex-end; /* Align buttons to the right */
@@ -23,40 +22,15 @@ const Button = styled.button`
 `;
 
 function Header() {
-    
     return (
-        <div 
-        className="navbar navbar-expand-lg navbar-light" 
-        style={{ backgroundColor: '#065446', color: '#EAE7B1', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-        <h1 
-        className="navbar-brand" 
-        style={{ color: '#EAE7B1', fontSize: '24px', fontWeight: 'bold', marginRight: 'auto' 
-        }}>
-            Culinary Quest
-        </h1>
-
-        <div className="collapse navbar-collapse" id="navBarSupportedContent">
-            <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                    <button 
-                    className="btn btn-outline-secondary mr-2 " 
-                    style={{ backgroundColor: '#EAE7B1', color: '#065446' 
-                    }}>
-                        Sign Up
-                    </button>
-                </li>
-                <li className="nav-item">
-                    <button 
-                    className="btn btn-outline-secondary" 
-                    style={{ backgroundColor: '#EAE7B1', color: '#065446' 
-                    }}>
-                        Login
-                    </button>
-                </li>
-            </ul>
+        <div className="navbar navbar-expand-lg navbar-light" style={{ backgroundColor: '#065446', color: '#EAE7B1', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+            <h1 className="navbar-brand" style={{ color: '#EAE7B1', fontSize: '24px', fontWeight: 'bold' }}>Culinary Quest</h1>
+            <ButtonSection>
+                <Button>Sign Up</Button>
+                <Button>Login</Button>
+            </ButtonSection>
         </div>
-    </div>
-);
+    );
 }
 
 export default Header;
